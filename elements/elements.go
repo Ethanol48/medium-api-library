cebc73b733d4a60ecfd686ce80fc54a5c0a79c56
Ethan Rouimi <contact@ethan-rouimi.com>
2023-11-12 01:51:23 +0100

    reorganize and implement ToHtml()

--- elements/elements.go
+++ elements/elements.go
@@ -3,21 +3,16 @@ package elements
 import (
 	"errors"
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/Ethanol48/medium-api/utilities"
 
 	"github.com/gocolly/colly"
 )
 
-type Markdown interface {
-	ToMarkdown() string
-}
-
 type Element interface {
 	GetName() string
 	ToMarkdown() string
+	ToHTML() string
 	addData(map[string][]string)
 }
 
@@ -54,33 +49,15 @@ type PlaceHolder struct {
 	Elem colly.HTMLElement
 }
 
-// implementations GetName()
+/* GetName() */
 
-func (elem *PlaceHolder) GetName() string {
-	return elem.Elem.Name
-}
+func (elem *PlaceHolder) GetName() string { return elem.Elem.Name }
+func (elem *List) GetName() string        { return elem.Name }
+func (elem *Image) GetName() string       { return elem.Name }
+func (elem *Title) GetName() string       { return elem.Name }
+func (elem *P) GetName() string           { return elem.Name }
 
-func (elem *List) GetName() string {
-	return elem.Name
-}
-
-func (elem *Image) GetName() string {
-	return elem.Name
-}
-
-func (elem *Title) GetName() string {
-	return elem.Name
-}
-
-func (elem *P) GetName() string {
-	return elem.Name
-}
-
-// addData
-
-func (l *PlaceHolder) addData(m map[string][]string) {
-	// elem.Content = m["content"]
-}
+/* addData */
 
 func (l *List) addData(m map[string][]string) {
 	for i := 0; i < len(m["content"]); i++ {
@@ -88,115 +65,10 @@ func (l *List) addData(m map[string][]string) {
 	}
 }
 
-func (elem *Image) addData(m map[string][]string) {
-	// elem.Name = m["name"]
-	// elem.Src = m["src"]
-	// elem.Alt = m["alt"]
-
-}
-
-func (elem *Title) addData(m map[string][]string) {
-	// elem.Name = m["name"]
-	// elem.Content = m["content"]
-}
-
-func (elem *P) addData(m map[string][]string) {
-	// elem.Name = m["name"]
-	// elem.Content = m["content"]
-}
-
-func (p P) ToMarkdown() string {
-	var sb strings.Builder
-
-	sb.WriteString(p.Content)
-	return sb.String()
-
-}
-
-func (i Image) ToMarkdown() string {
-	var sb strings.Builder
-
-	lin := i.Base + "/" + i.Id
-
-	sb.WriteString(fmt.Sprintf("![%s](%s)", i.Alt, lin))
-	return sb.String()
-
-}
-
-func (t Title) ToMarkdown() string {
-	var sb strings.Builder
-
-	switch t.Name {
-	case "h1":
-		sb.WriteString("# ")
-
-	case "h2":
-		sb.WriteString("## ")
-
-	case "h3":
-		sb.WriteString("### ")
-
-	case "h4":
-		sb.WriteString("#### ")
-	}
-
-	sb.WriteString(t.Content)
-
-	return sb.String()
-
-}
-
-func (l List) ToMarkdown() string {
-	var sb strings.Builder
-	var prefix string
-
-	switch l.Name {
-	case "ol":
-		prefix = "numeric"
-
-	case "ul":
-		prefix = ""
-
-	}
-
-	for i, v := range l.Compts {
-		if prefix == "numeric" {
-			sb.WriteString(fmt.Sprintf("%v. %s", i, v))
-		} else {
-			sb.WriteString(fmt.Sprintf("- %s", v))
-		}
-		sb.WriteString("\n")
-	}
-
-	return sb.String()
-}
-
-func (p PlaceHolder) ToMarkdown() string {
-	return "null"
-}
-
-func (a Article) ToMarkdown() string {
-	var sb strings.Builder
-
-	for _, v := range a.Content {
-		sb.WriteString(v.ToMarkdown())
-		sb.WriteString("\n\n")
-
-	}
-
-	return sb.String()
-}
-
-func (a Article) ToMarkdownFile(path string) {
-
-	err := os.WriteFile(path, []byte(a.ToMarkdown()), 0644) // 0644 is a common permission setting allowing reading for everyone and full write access to the owner of the file.
-	if err != nil {
-		// Handle the error here
-		fmt.Println("Error writing to file: ", err)
-	}
-
-	fmt.Println("File created at: ", path)
-}
+func (l *PlaceHolder) addData(m map[string][]string) {}
+func (elem *Image) addData(m map[string][]string)    {}
+func (elem *Title) addData(m map[string][]string)    {}
+func (elem *P) addData(m map[string][]string)        {}
 
 func createElement(elem *colly.HTMLElement) (Element, error) {
 
@@ -316,13 +188,8 @@ func ExtractDataArticle(elem *colly.HTMLElement, idx int) Element {
 		e = &i
 
 	} else {
-		// tiene que ser o un header o un paragraph
-
-		// fmt.Println("The element we are analizing: ")
-		// fmt.Printf("%+v\n\n", elem)
-
+		// it's another permitted element (header or paragragh)
 		if elem.Name == "p" {
-
 			p := P{
 				Name:    elem.Name,
 				Content: utilities.TrimMoreThanOneSpace(elem.Text),
@@ -331,12 +198,10 @@ func ExtractDataArticle(elem *colly.HTMLElement, idx int) Element {
 			e = &p
 
 		} else {
-
 			h := Title{
 				Name:    elem.Name,
 				Content: utilities.TrimMoreThanOneSpace(elem.Text),
 			}
-
 			e = &h
 		}
 	}
---
+++ elements/formating.go
@@ -0,0 +1,185 @@
+package elements
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+/* Markdown */
+
+func (p P) ToMarkdown() string {
+	var sb strings.Builder
+
+	sb.WriteString(p.Content)
+	return sb.String()
+
+}
+
+func (i Image) ToMarkdown() string {
+	var sb strings.Builder
+
+	lin := i.Base + "/" + i.Id
+
+	sb.WriteString(fmt.Sprintf("![%s](%s)", i.Alt, lin))
+	return sb.String()
+
+}
+
+func (t Title) ToMarkdown() string {
+	var sb strings.Builder
+
+	switch t.Name {
+	case "h1":
+		sb.WriteString("# ")
+
+	case "h2":
+		sb.WriteString("## ")
+
+	case "h3":
+		sb.WriteString("### ")
+
+	case "h4":
+		sb.WriteString("#### ")
+	}
+
+	sb.WriteString(t.Content)
+
+	return sb.String()
+
+}
+
+func (l List) ToMarkdown() string {
+	var sb strings.Builder
+	var prefix string
+
+	switch l.Name {
+	case "ol":
+		prefix = "numeric"
+
+	case "ul":
+		prefix = ""
+
+	}
+
+	for i, v := range l.Compts {
+		if prefix == "numeric" {
+			sb.WriteString(fmt.Sprintf("%v. %s", i, v))
+		} else {
+			sb.WriteString(fmt.Sprintf("- %s", v))
+		}
+		sb.WriteString("\n")
+	}
+
+	return sb.String()
+}
+
+func (p PlaceHolder) ToMarkdown() string {
+	return "null"
+}
+
+func (a Article) ToMarkdown() string {
+	var sb strings.Builder
+
+	for _, v := range a.Content {
+		sb.WriteString(v.ToMarkdown())
+		sb.WriteString("\n\n")
+
+	}
+
+	return sb.String()
+}
+
+func (a Article) ToMarkdownFile(path string) {
+
+	err := os.WriteFile(path, []byte(a.ToMarkdown()), 0644) // 0644 is a common permission setting allowing reading for everyone and full write access to the owner of the file.
+	if err != nil {
+		// Handle the error here
+		fmt.Println("Error writing to file: ", err)
+	}
+
+	fmt.Println("File created at: ", path)
+}
+
+func (a Article) ToHtmlFile(path string) {
+
+	err := os.WriteFile(path, []byte(a.ToHTML()), 0644) // 0644 is a common permission setting allowing reading for everyone and full write access to the owner of the file.
+	if err != nil {
+		// Handle the error here
+		fmt.Println("Error writing to file: ", err)
+	}
+
+	fmt.Println("File created at: ", path)
+}
+
+/* HTML */
+
+// can you write for me the ToHTML method squeleton for each type in this file that implements the interface Element
+func (p P) ToHTML() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("<p>%s</p>", p.Content))
+
+	return sb.String()
+
+}
+
+func (i Image) ToHTML() string {
+	var sb strings.Builder
+
+	sb.WriteString(fmt.Sprintf("<img src='%s/%s' alt='%s'>", i.Base, i.Id, i.Alt))
+	return sb.String()
+}
+
+func (t Title) ToHTML() string {
+	var sb strings.Builder
+
+	switch t.Name {
+	case "h1":
+		sb.WriteString(fmt.Sprintf("<h1>%s</h1>", t.Content))
+	case "h2":
+		sb.WriteString(fmt.Sprintf("<h2>%s</h2>", t.Content))
+	case "h3":
+		sb.WriteString(fmt.Sprintf("<h3>%s</h3>", t.Content))
+	case "h4":
+		sb.WriteString(fmt.Sprintf("<h4>%s</h4>", t.Content))
+	}
+
+	return sb.String()
+}
+
+func (l List) ToHTML() string {
+	var sb strings.Builder
+	var tmp string
+	var wrappingTags string
+
+	switch l.Name {
+	case "ol":
+		wrappingTags = "<ol>%s</ol>"
+	case "ul":
+		wrappingTags = "<ul>%s</ul>"
+	}
+
+	for _, v := range l.Compts {
+		tmp = tmp + fmt.Sprintf("<li>%s</li>", v)
+	}
+
+	sb.WriteString(fmt.Sprintf(wrappingTags, tmp))
+	return sb.String()
+}
+
+func (p PlaceHolder) ToHTML() string {
+	var sb strings.Builder
+
+	sb.WriteString(fmt.Sprintf("<div>%s</div>", p.ToMarkdown()))
+	return sb.String()
+}
+
+func (a Article) ToHTML() string {
+	var sb strings.Builder
+
+	for i := 0; i < len(a.Content); i++ {
+		sb.WriteString(a.Content[i].ToHTML())
+	}
+
+	return sb.String()
+}
