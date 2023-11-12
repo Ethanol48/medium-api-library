b89fe236ff7b9f5afb7b57c3ded488c4e1133a35
Ethan Rouimi <contact@ethan-rouimi.com>
2023-11-12 01:51:57 +0100

    cleaner

--- article/article.go
+++ article/article.go
@@ -7,65 +7,64 @@ import (
 	"github.com/gocolly/colly"
 )
 
-func GetArticle(h *colly.HTMLElement) elements.Article {
+func GetArticle(link string) elements.Article {
 
-	var header *colly.HTMLElement
+	c := colly.NewCollector()
+	// c.WithTransport(t)
+
 	var art elements.Article
 
-	// nodes := h.DOM.Nodes
-	h.ForEach("div", func(i int, e *colly.HTMLElement) {
+	c.OnHTML("section > div > div:nth-child(2) > div > div", func(h *colly.HTMLElement) {
 
-		if i == 0 {
-			header = e
-		}
+		var header *colly.HTMLElement
 
-		// fmt.Printf("e with number %v: %v\n", i, e.Name)
+		var permElems []string
 
-		// fmt.Printf("\nPrinting number: %v\n", i)
-		// fmt.Printf("e.Name: %v\n", e.Name)
-	})
+		// add permite elements
+		permElems = append(permElems, "h1")
+		permElems = append(permElems, "h2")
+		permElems = append(permElems, "h3")
+		permElems = append(permElems, "ul")
+		permElems = append(permElems, "ol")
+		permElems = append(permElems, "p")
+		permElems = append(permElems, "li")
+		permElems = append(permElems, "img")
 
-	var permElems []string
+		h.ForEach("*:not(:first-child)", func(i int, e *colly.HTMLElement) {
 
-	// add permite elements
-	permElems = append(permElems, "h1")
-	permElems = append(permElems, "h2")
-	permElems = append(permElems, "h3")
-	permElems = append(permElems, "ul")
-	permElems = append(permElems, "ol")
-	permElems = append(permElems, "p")
-	permElems = append(permElems, "li")
-	permElems = append(permElems, "img")
+			if utilities.StringInArray(e.Name, permElems) {
+				// fmt.Printf("e with number %v: %v\n", i, e.Name)
+				// fmt.Printf("%s \n\n", trimMoreThanOneSpace(e.Text))
 
-	h.ForEach("*:not(:first-child)", func(i int, e *colly.HTMLElement) {
+				result := elements.ExtractDataArticle(e, i)
 
-		if utilities.StringInArray(e.Name, permElems) {
-			// fmt.Printf("e with number %v: %v\n", i, e.Name)
-			// fmt.Printf("%s \n\n", trimMoreThanOneSpace(e.Text))
-
-			result := elements.ExtractDataArticle(e, i)
-
-			if result.GetName() != "" {
-				art.Content = append(art.Content, result)
+				if result.GetName() != "" {
+					art.Content = append(art.Content, result)
+				}
 			}
-		}
 
-		// fmt.Printf("\nPrinting number: %v\n", i)
-		// fmt.Printf("e.Name: %v\n", e.Name)
+			// fmt.Printf("\nPrinting number: %v\n", i)
+			// fmt.Printf("e.Name: %v\n", e.Name)
+
+		})
+
+		headerSelection := h.DOM.Find("div").First()
+		header = &colly.HTMLElement{}
+		header.DOM = headerSelection
+
+		art.Title = header.ChildText(`h1[data-testid="storyTitle"]`)
+		art.ReadTime = header.ChildText(`span[data-testid="storyReadTime"]`)
+		art.Publisehd = header.ChildText(`span[data-testid="storyPublishDate"]`)
 
 	})
 
-	art.Title = header.ChildText(`h1[data-testid="storyTitle"]`)
-	art.ReadTime = header.ChildText(`span[data-testid="storyReadTime"]`)
-	art.Publisehd = header.ChildText(`span[data-testid="storyPublishDate"]`)
-
-	// fmt.Printf("before: %#v \n\n", before)
-
-	// err := os.WriteFile("Article.md", []byte(art.ToMarkdown()), 0644) // 0644 is a common permission setting allowing reading for everyone and full write access to the owner of the file.
-	// if err != nil {
-	// 	// Handle the error here
-	// 	fmt.Println("Error writing to file:", err)
-	// }
+	if link == "test" {
+		go utilities.SpinUp("testing")
+		c.Visit("http://localhost:8080/")
+	} else {
+		c.Visit(link)
+	}
+	c.Wait()
 
 	return art
 
